6f9d40a69658e6cc4adcbf9bca8b4706f35e570e
Franta1205 <hoza.fr@gmail.com>
2024-11-02 13:33:05 +0100

    create and edit

--- app/controllers/cv_controller.rb
+++ app/controllers/cv_controller.rb
@@ -1,19 +1,35 @@
 class CvController < ApplicationController
-  before_action :authenticate_user!, only: %i[home new create show]
+  before_action :authenticate_user!, except: %i[home show]
   def home
   end
 
   def new
+    redirect_to edit_cv_path(id: current_user.cv.id) if current_user.cv
+    @cv = Cv.new
   end
 
   def create
+    @cv = Cv.new(cv_params)
+    @cv.user_id = current_user.id
+
+    if @cv.save
+      redirect_to edit_cv_path(id: @cv.id), notice: 'CV was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @cv = Cv.find(params[:id])
   end
 
   def show
 
+  end
+
+  private
+
+  def cv_params
+    params.require(:cv).permit(:content)
   end
 end
