5406105bc077ce88ba3cd2f2bc027b9c8883c595
Franta1205 <hoza.fr@gmail.com>
2024-11-03 14:40:07 +0100

    fix redirecting

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,11 +4,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      sign_in @user, event: :authentication
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Github"
+      redirect_to edit_your_cv_cv_index_path
     else
       session["devise.github_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      redirect_to root_path, alert: @user.errors.full_messages.join("\n")
     end
   end
 end
