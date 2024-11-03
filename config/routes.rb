3d8d466b9aabac457ee1f306b2b0da771016db6c
Franta1205 <hoza.fr@gmail.com>
2024-11-03 14:48:49 +0100

    editin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, skip: [:registrations, :sessions]
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   root "cv#home"
   resources :cv do
     collection do
